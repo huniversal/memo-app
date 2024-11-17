d213fcdc072c8a7e0371b320d9e50409c17cc5b4
huniversal <hunivers0523@naver.com>
2024-11-18 00:14:49 +0900

    App.jsx 페이지 라우터 구성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,32 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Route } from 'react-router'
+import Header from './components/Header'
+
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import MemoPage from './pages/MemoPage';
+import TodoPage from './pages/TodoPage';
+import MyPage from './pages/MyPage';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        {/* Body위 컨텐츠 박스 */}
+        <div className='content-box'>
+          {/* 상단 Nav */}
+          <Header />
+          <div className='content'>
+            <SideBar />
+            {/* MainPage : 라우팅으로 페이지 전환 */}
+            <main className='main'>
+              <Routes>
+                <Route path='/' element={<MemoPage />} />
+                <Route path='/' element={<TodoPage />} />
+                <Route path='/' element={<MyPage />} />
+              </Routes>
+            </main>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
