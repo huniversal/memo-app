40640ceed54f444592eae625cfe77e5a0275a077
huniversal <hunivers0523@naver.com>
2024-11-18 00:31:01 +0900

    body 레이아웃 구조 확인 및 스타일 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route } from 'react-router'
-import Header from './components/Header'
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// 컴포넌트 import
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import MemoPage from './pages/MemoPage';
-import TodoPage from './pages/TodoPage';
-import MyPage from './pages/MyPage';
-
+import MemoPage from './components/MemoPage';
+import TodoPage from './components/TodoPage';
+import MyPage from './components/MyPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className='App'>
+    <Router>
+      <div className="App">
         {/* Body위 컨텐츠 박스 */}
-        <div className='content-box'>
+        <div className="content-box">
           {/* 상단 Nav */}
           <Header />
-          <div className='content'>
-            <SideBar />
+          <div className="content">
+            <Sidebar />
             {/* MainPage : 라우팅으로 페이지 전환 */}
-            <main className='main'>
+            <main className="main">
               <Routes>
-                <Route path='/' element={<MemoPage />} />
-                <Route path='/' element={<TodoPage />} />
-                <Route path='/' element={<MyPage />} />
+                <Route path="/" element={<MemoPage />} />
+                <Route path="/todo" element={<TodoPage />} />
+                <Route path="/mypage" element={<MyPage />} />
               </Routes>
             </main>
           </div>
         </div>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
