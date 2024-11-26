aa9250ba68a83c5d402cf00c4313eaafc2715cde
huniversal <hunivers0523@naver.com>
2024-11-27 00:46:27 +0900

    캘린더에 일정 추가하기

--- frontend/src/components/Todo/CustomCalendar.jsx
+++ frontend/src/components/Todo/CustomCalendar.jsx
@@ -8,21 +8,36 @@ import TodoSidebar from './TodoSidebar';
 const localizer = momentLocalizer(moment);
 const CustomCalendar = () => {
   const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
   const toggleOffanvas = () => {
     setShowOffcanvas(!showOffcanvas);
     console.log("Sidebar visibility:", !showOffcanvas);
   }
+
+  const handleAddEvent = (newEvent) => {
+    setEvents([...events, newEvent]);
+    setShowOffcanvas(fasle);
+  }
+
   return (
     <div className='calendar-container'>
       <div className='btn-container'>
-        <button className='btn-event' onClick={toggleOffanvas}>
+        <button className='btn-event' onClick={()=> {setSelectedEvent(null); toggleOffanvas();}}>
           투두리스트 추가
         </button>
-        <TodoSidebar show={showOffcanvas} onHide={toggleOffanvas}/>
+        <TodoSidebar 
+          show={showOffcanvas} 
+          onHide={toggleOffanvas}
+          onAddEvent={handleAddEvent}
+          selectedEvent={selectedEvent}  
+        />
+
       </div>
       <Calendar
         localizer={localizer}
+        events={events}
         startAccessor="start"
         endAccessor="end"
         style={{height:'70vh', width: "90%"}}
--- frontend/src/components/Todo/TodoSidebar.jsx
+++ frontend/src/components/Todo/TodoSidebar.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import moment from 'moment';
+import React, { useState } from 'react'
 import "../Style/TodoSidebar.css";
 
-const TodoSidebar = ({show, onHide}) => {
+const TodoSidebar = ({show, onHide, onAddEvent, seletedEvent}) => {
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [startTime, setStartTime] = useState('');
+  const [endTime, setEndTime] = useState('');
+
+  const handleSubmit = () => {
+    const newEvents = {
+      id: Math.random(), 
+      title, 
+      start: moment(date).set({hour:startTime.split(':')[0], minute:startTime.split(':')[1]}).toDate(),
+      end : moment(date).set({hour:endTime.split(':')[0], minute:endTime.split(':')[1]}).toDate(),
+    };
+    onAddEvent(newEvents);
+  };
+
   return (
     <div className={`Todo-sidebar ${show ? 'show' : ''}`}>
       {console.log("Sidebar class:", show ? 'show' : '')}
@@ -9,6 +25,37 @@ const TodoSidebar = ({show, onHide}) => {
         <div className='Todo-Sidebar-header'>
           <h3>투두리스트 추가</h3>
           <button className="close-btn" onClick={onHide}>닫기</button>
+        </div>
+        <div className='sidebar-body'>
+          <label>Title</label>
+          <input 
+            type='text'
+            placeholder='Event Title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Date</label>
+          <input 
+            type='date'
+            placeholder='Event Title'
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <label>Start Time</label>
+          <input 
+            type='time'
+            placeholder='Event Title'
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)}
+          />
+          <label>End Time</label>
+          <input 
+            type='time'
+            placeholder='Event Title'
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)}
+          />
+          <button className='btn-primary' onClick={handleSubmit}>Submit</button>
         </div>
       </div>
     </div>
