900c2233bd6e03cb20433cc906f68390e632fdca
huniversal <hunivers0523@naver.com>
2024-11-26 18:22:42 +0900

    투두리스트 React-Big-Calendar 라이브러리 사용

---
+++ frontend/src/components/Todo/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Calendar, View, DateLocalizer } from 'react-big-calendar';
+import moment from "moment";
+import { momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const Calendar = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Calendar
--- frontend/src/components/Todo/TodoPage.jsx
+++ frontend/src/components/Todo/TodoPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import TodoCalender from "./TodoCalender";
+
 
 const TodoPage = () => {
 
   return (
     <div>
-      <TodoCalender />
+
     </div>
   )
 }
