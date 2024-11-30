d23c03ef155230a82a64e45122630059f2feedf3
huniversal <hunivers0523@naver.com>
2024-11-30 19:42:01 +0900

    Todo Sidebar 에러 해결

--- frontend/src/components/Todo/TodoSidebar.jsx
+++ frontend/src/components/Todo/TodoSidebar.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import "../Style/TodoSidebar.css";
 
 const TodoSidebar = ({ show, onHide, events }) => {
-  if (!show) {
-    console.log("Sidebar is Hidden");
-    return null;
-  }
-  console.log("Sidebar is visible");
+  console.log(show ? "Sidebar is visible" : "Sidebar is Hidden");
+
   return (
-    <div className="Todo-sidebar">
+    <div className={`Todo-sidebar ${show ? "show" : ""}`}>
       <div className="Todo-Sidebar-Content">
         <div className="Todo-Sidebar-header">
           <h3>일정 리스트</h3>
-          <button className="close-btn" onClick={onHide}>닫기</button>
+          <button className="close-btn" onClick={onHide}>
+            닫기
+          </button>
         </div>
         <ul className="event-list">
           {events.map((event) => (
